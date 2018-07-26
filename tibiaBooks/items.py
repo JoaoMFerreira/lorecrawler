d312ac4fd4dfe19f0bdd422b90ae4c68724b0964
JoaoMarcos <joao_webb@hotmail.com>
2018-07-25 22:55:49 -0300

    Alteração nos scripts

--- tibiaBooks/items.py
+++ tibiaBooks/items.py
@@ -10,11 +10,15 @@ from scrapy.loader.processors import TakeFirst
 
 
 def lower_case(value):
-    value.lower()
+    if( isinstance(value[0], str) ):
+        return value[0].lower()
+    else:
+        return value[0]
+        pass    
 
 class TibiabooksItem(scrapy.Item):
     # define the fields for your item here like:
-    id = scrapy.Field()
+    id = scrapy.Field(input_processor=lower_case, output_processor=TakeFirst())
     name = scrapy.Field(input_processor=lower_case, output_processor=TakeFirst())
     location = scrapy.Field(input_processor=lower_case, output_processor=TakeFirst())
     author = scrapy.Field(input_processor=lower_case, output_processor=TakeFirst())
--- tibiaBooks/spiders/books.py
+++ tibiaBooks/spiders/books.py
@@ -2,6 +2,8 @@ import scrapy
 from tibiaBooks.items import TibiabooksItem
 from scrapy.loader import ItemLoader
 
+id = 0
+
 class BooksSpider(scrapy.Spider):
     name = "booksT"
     start_urls = [
@@ -9,16 +11,17 @@ class BooksSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for libraries in response.xpath('//li/a[contains(@href,"_Librar")]/@href').extract_first():
-            yield response.follow(libraries, callback=self.parse_library)
+        for library in response.xpath('//li/a[contains(@href,"_Librar")]/@href').extract():
+            yield response.follow(library, callback=self.parse_library)
 
     def parse_library(self, response):
-        for book in response.xpath('//a[contains(@href,"(Book)")]/@href').extract_first():
+        for book in response.xpath('//a[contains(@href,"(Book)")]/@href').extract():
             yield response.follow(book, callback=self.parse_book)
 
-    def parse_book(self, response):
-        id = 0
-        id = id + 1
+    def parse_book(self, response):      
+        global id
+        id += 1
+        
         location = response.css('aside div.pi-data-value a::text')[0].extract()
         author = response.css('aside div.pi-data-value::text')[1].extract()
         shortDescription = response.css('aside div.pi-data-value::text')[0].extract()
@@ -30,6 +33,6 @@ class BooksSpider(scrapy.Spider):
         l.add_value('location', location)
         l.add_value('author', author)
         l.add_value('shortDescription', shortDescription)
-        l.add_value('text', shortDescription)
-        return l.load_item()
+        l.add_value('text', text)
+        return l.load_item()   
 
